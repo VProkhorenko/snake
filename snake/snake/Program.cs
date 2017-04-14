267b9479cc454574341fa834393526edfce64110
VProkhorenko <wowa2k@gmail.com>
2017-04-14 20:45:33 +0300

    передаем значения конструктору

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,17 +10,21 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
+            Point p1 = new Point(1, 3, '*');
+           /*
+             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
+            */
             // Draw(p1.x, p1.y, p1.sym);
             p1.Draw();
 
-            Point p2 = new Point();
+            Point p2 = new Point(4, 5, '#');
+            /*
             p2.x = 4;
             p2.y = 5;
             p2.sym = '#';
+            */
             // Draw(p2.x, p2.y, p2.sym);
             p2.Draw();
 
