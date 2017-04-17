391448265b91a51df7ef1ee938c002534606f1f4
VProkhorenko <wowa2k@gmail.com>
2017-04-17 09:42:22 +0300

    отрисовка рамки

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,14 +10,26 @@ namespace snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(80, 25);
+
+            
+            
+            //отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+            // отрисовка точек
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-            
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
 
             Console.ReadLine();
         }
