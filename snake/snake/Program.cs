3fcd59a856d582bf2f654491a81340b982eafbe8
VProkhorenko <wowa2k@gmail.com>
2017-04-18 11:39:43 +0300

    добавили многократный запуск метода Перемещения

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace snake
@@ -32,7 +33,24 @@ namespace snake
             // p.Draw();
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+
             Console.ReadLine();
         }
 
